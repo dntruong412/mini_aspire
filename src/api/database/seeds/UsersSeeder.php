fede6085e5d385062efcab754a8fcd06510fad39
Truong Dinh <truongdinh@logflows.com>
2021-06-15 02:42:18 +0700

    update db seeding

--- src/api/database/seeds/UsersSeeder.php
+++ src/api/database/seeds/UsersSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class UsersSeeder extends Seeder
 {
@@ -12,19 +11,6 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        $date = new \DateTime();
-
-        $users = [];
-        for ($i=1; $i < 50; $i++) { 
-            $users[] = [
-                'name'       => "User $i",
-                'created_at' => $date
-            ];
-        }
-
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-        DB::table('users')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
-        DB::table('users')->insert($users);
+        factory(App\User::class, 50)->create();
     }
 }
